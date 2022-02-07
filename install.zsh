0568da83a6dd16e522cef61f0abf36c66b72a9be
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2022-02-07 17:19:35 +0100

    Fix installation script (#1)
    Typos, set -e compliance, order, submodules and then some.

--- install.zsh
+++ install.zsh
@@ -1,4 +1,4 @@
-set -e
+set -xe
 
 die() {
 	>&2 echo $1
@@ -7,22 +7,27 @@ die() {
 
 [[ -n "$ZSH_VERSION" ]] || die 'Please use zsh'
 
-[[ d- "$HOME/.oh-my-zsh" ]] || sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+[[ -d "$HOME/.oh-my-zsh" ]] || {
+	echo "⚠️ Installing Oh my ZSH, you’ll have to exit it to continue the installation process"
+	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+}
 type brew > /dev/null || /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-
-brew bundle # See ~/Brewfile
 
 function dotfiles {
    /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
 }
 
-	git clone --bare git@github.com:BuonOmo/dotfiles.git $HOME/.dotfiles
-	mkdir -p .dotfiles.old
-	dotfiles checkout
-
-	if [[ $? != 0 ]]; then
-		dotfiles checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .dotfiles.old/{}
-	fi;
+	git clone --bare git@github.com:BuonOmo/dotfiles.git $HOME/.dotfiles # If failing here, see https://docs.github.com/en/authentication/connecting-to-github-with-ssh
+	
+	# Backing up
+	mkdir -p $HOME/.dotfiles.old
+	for new_file in $(dotfiles ls-tree --full-tree -r --name-only HEAD); do
+		[[ -f "$HOME/$new_file" ]] || continue
+		
+		echo "Backing up `~/$new_file` to `~/.dotfiles.old/$new_file`."
+		mv "$new_file" ".dotfiles.old/$new_file"
+	done
+	[[ -z "$(ls -A $HOME/.dotfiles.old)" ]] && rm -d $HOME/.dotfiles.old
 
 	dotfiles checkout
 	dotfiles config status.showUntrackedFiles no
@@ -33,3 +38,5 @@ function dotfiles {
    done
 
 unfunction dotfiles
+
+brew bundle # See ~/Brewfile
